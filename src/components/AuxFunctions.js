7a58f540959e8a1e5ad66c4777a42a203b13f7c9
leo <celsolnv27@gmail.com>
2021-12-08 14:20:40 -0300

    Preparando revisão de código

--- src/components/AuxFunctions.js
+++
@@ -1,5 +0,0 @@
-export default function InsertIcon({ name , classname }){
-    return(
-        <ion-icon name={name} class={classname} ></ion-icon>
-    );
-}
--- src/components/LikeSection.js
+++
@@ -1,8 +0,0 @@
-export default function LikeSection({firstLikeImg , firstLikeName, numberLike}){
-    return (
-        <div class="like">
-            <button><img src={firstLikeImg}/></button>
-            <p>Curtido por <strong>{firstLikeName}</strong> e <strong>outras {numberLike - 1} pessoas</strong></p>
-        </div>
-    );
-}
